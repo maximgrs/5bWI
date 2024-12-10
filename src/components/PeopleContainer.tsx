0184f4441494a751996eebd1315149d99ca28ef8
Maxim <maksimfhmad@gmail.com>
2024-12-10 10:08:49 +0100

    fetch eingebaut

--- src/components/PeopleContainer.tsx
+++ src/components/PeopleContainer.tsx
@@ -1,47 +1,40 @@
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
+import { Directus } from "@directus/sdk";
 
-type Props = object;
+const directus = new Directus("http://10.115.1.39:8055");
 
-// eslint-disable-next-line no-empty-pattern
-export default function PeopleContainer({}: Props) {
+export default function PeopleContainer() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await directus.auth.login({
+          email: "admin@example.com",
+          password: "d1r3ctu5",
+        });
+
+        const response = await directus.items("students").readByQuery();
+        setPeople(response.data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-8">
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
-      <Card
-        name="Sefa Öztürk"
-        description="top schüler akdfjadf alkdfj aslökd nbankjdk fjaölsjkdf ajkldf andkf"
-        age={21}
-        imageUrl="https://picsum.photos/200/300"
-      />
+      {people.map((person) => (
+        <Card
+          name={person.name}
+          description={person.description}
+          age={person.age}
+          imageUrl={person.imageUrl}
+        />
+      ))}
     </div>
   );
 }
